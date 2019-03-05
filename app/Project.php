cb34574f4f5b41a98571027e9fde71bd67039b02
marko2107 <marko.drnasin@gmail.com>
2019-03-05 20:48:07 +0100

    dodan task model, factory i migracija

--- app/Project.php
+++ app/Project.php
@@ -6,8 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
+  public function tasks(){
+    return $this->hasMany(Task::class);
+  }
+
     //protected $guarded=[];
      protected $fillable = [
       'title','description'
-     ];
+    ];
 }
+
+v16 04:39m
